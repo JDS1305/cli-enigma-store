2296bbe071c5418ef9a56df246b7842aa81339e6
Lely Ruswitasari <lelyr1305@gmail.com>
2024-02-15 09:00:19 +0700

    feat: Add Interface ProductService

---
+++ src/ProductService.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface ProductService {
+    void addProduct (Product product);
+    void updateProduct (int id, Product updatedProduct);
+    void deleteProduct (int id);
+    List<Product> getAllProducts();
+    List<Product> searchProducts(String keyword);
+}
